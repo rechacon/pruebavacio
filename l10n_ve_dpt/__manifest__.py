381a232396deaaa0c3141e5cf73eadf122ec210a
Franyer Hidalgo <fhidalgo.dev@gmail.com>
2021-11-25 06:47:52 -0400

    ADD: l10n_ve_dpt

---
+++ l10n_ve_dpt/__manifest__.py
@@ -0,0 +1,44 @@
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2010 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    "name": "Localización Venezolana: Municipios y Parroquias",
+    "version": "0.3",
+    "author": "BACHACO-VE",
+    "category": "Localization",
+    'contributors': ['Franyer Hidalgo <fhidalgo.dev@gmail.com>'],
+    "website": "http://www.bachaco.org.ve",
+    'images': ['static/description/icon.png'],
+    "depends": ['base',
+                'contacts',
+                ],
+    "init_xml": [],
+    "demo_xml": [],
+    "data": [
+        'security/ir.model.access.csv',
+        'data/res.country.state.xml',
+        'data/res.country.state.municipality.xml',
+        'data/res.country.state.municipality.parish.xml',
+        'views/l10n_ve_dpt_view.xml',
+        'views/res_partner.xml',
+        'data/res_country.xml',
+    ],
+    "installable": True
+}
---
+++ l10n_ve_dpt/models/res_partner.py
@@ -0,0 +1,15 @@
+from odoo import models, fields, api
+
+
+class ResPartner(models.Model):
+    _inherit = 'res.partner'
+
+    municipality_id = fields.Many2one('res.country.state.municipality', 'Municipality')
+    parish_id = fields.Many2one('res.country.state.municipality.parish', 'Parish')
+
+    @api.model
+    def _address_fields(self):
+        address_fields = set(super(ResPartner, self)._address_fields())
+        address_fields.add('municipality_id')
+        address_fields.add('parish_id')
+        return list(address_fields)
